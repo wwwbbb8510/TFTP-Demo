9fe76d04e1bd659b0d8e4591b19b6a7a6954d788
steven <steven.wang@foundryhq.com>
2015-08-18 13:34:37 +1200

    develop server and client

--- src/com/bgoverseas/TFTPClientTest.java
+++ src/com/bgoverseas/TFTPClientTest.java
@@ -13,31 +13,38 @@ import java.net.UnknownHostException;
 
 /**
  * Created by binwang on 17/08/15.
+ * TFTP client used for test
  */
 public class TFTPClientTest {
+    /**
+     * help text for users
+     */
     public static final String HELP_TEXT =
-            "Usage: tftp [options] hostname localfile remotefile\n\r" +
-                    "hostname   - The name of the remote host\n" +
-                    "localfile  - The name of the local file\n" +
-                    "remotefile - The name of the remote file\n" +
+            "Usage: TFTPClientTest [options] hostname local_file remote_file\n\r" +
+                    "hostname   - The name of the server\n" +
+                    "local_file  - The client file name\n" +
+                    "remote_file - The server file name\n" +
                     "options: (The default is to assume -r -b)\n" +
                     "\t-s Send a local file\n" +
                     "\t-r Receive a remote file\n" +
                     "\t-a Use Netascii transfer mode\n" +
                     "\t-b Use Octet transfer mode\n";
 
-    public static void main(String[] args)
-    {
-        boolean receiveFile = true, closed;
-        String mode = TFTPBase.OCTET_MODE;
-        int argc;
+    /**
+     * entry of client test
+     * @param args
+     */
+    public static void main(String[] args) {
+        boolean receiveFile = true;//receive a remote file as default
+        String mode = TFTPBase.OCTET_MODE;//octet as the default mode
+        boolean closed;
+        int argCounter;
         String arg, hostname, localFilename, remoteFilename;
         TFTPClient tftpClient;
 
         // Parse options
-        for (argc = 0; argc < args.length; argc++)
-        {
-            arg = args[argc];
+        for (argCounter = 0; argCounter < args.length; argCounter++) {
+            arg = args[argCounter];
             if (arg.startsWith("-"))
             {
                 if (arg.equals("-r")) {
@@ -49,7 +56,7 @@ public class TFTPClientTest {
                 } else if (arg.equals("-b")) {
                     mode = TFTPBase.OCTET_MODE;
                 } else {
-                    System.err.println("Error: unrecognized option.");
+                    System.err.println("Error: wrong option.");
                     System.err.print(TFTPClientTest.HELP_TEXT);
                     System.exit(1);
                 }
@@ -58,62 +65,45 @@ public class TFTPClientTest {
             }
         }
 
-        // Make sure there are enough arguments
-        if (args.length - argc != 3)
-        {
-            System.err.println("Error: invalid number of arguments.");
+        //Parse the hostname, local_file, remote_file
+        if (args.length - argCounter != 3) {
+            System.err.println("Error: There must be three arguments after the options");
             System.err.print(TFTPClientTest.HELP_TEXT);
             System.exit(1);
         }
+        hostname = args[argCounter];
+        localFilename = args[argCounter + 1];
+        remoteFilename = args[argCounter + 2];
 
-        // Get host and file arguments
-        hostname = args[argc];
-        localFilename = args[argc + 1];
-        remoteFilename = args[argc + 2];
-
-        // Create our TFTP instance to handle the file transfer.
-        tftpClient = new TFTPClient();
-
-        // We want to timeout if a response takes longer than 60 seconds
-        tftpClient.setTimeout(60000);
-
-        // Open local socket
-        try
-        {
+        //run the TFTP client to send or request a file
+        tftpClient = new TFTPClient();// Create a TFTPClient instance to handle the file transfer.
+        tftpClient.setTimeout(60000);// Set the timeout to 1 minute
+        try {
             tftpClient.openSocket();
         }
-        catch (SocketException e)
-        {
+        catch (SocketException e) {
             System.err.println("Error: could not open local UDP socket.");
             System.err.println(e.getMessage());
             System.exit(1);
         }
-
-        // We haven't closed the local file yet.
-        closed = false;
-
-        // If we're receiving a file, receive, otherwise send.
-        if (receiveFile)
-        {
+        closed = false;//set the closed status of the local file to false which means it's open
+        if (receiveFile) {//receive a file
             FileOutputStream output = null;
             File file;
 
             file = new File(localFilename);
 
             // If file exists, don't overwrite it.
-            if (file.exists())
-            {
+            if (file.exists()) {
                 System.err.println("Error: " + localFilename + " already exists.");
                 System.exit(1);
             }
 
             // Try to open local file for writing
-            try
-            {
+            try {
                 output = new FileOutputStream(file);
             }
-            catch (IOException e)
-            {
+            catch (IOException e) {
                 tftpClient.close();
                 System.err.println("Error: could not open local file for writing.");
                 System.err.println(e.getMessage());
@@ -121,36 +111,30 @@ public class TFTPClientTest {
             }
 
             // Try to receive remote file via TFTP
-            try
-            {
+            try {
                 tftpClient.receiveFile(remoteFilename, mode, output, InetAddress.getByName(hostname));
             }
-            catch (UnknownHostException e)
-            {
+            catch (UnknownHostException e) {
                 System.err.println("Error: could not resolve hostname.");
                 System.err.println(e.getMessage());
                 System.exit(1);
             }
-            catch (IOException e)
-            {
+            catch (IOException e) {
                 System.err.println(
                         "Error: I/O exception occurred while receiving file.");
                 System.err.println(e.getMessage());
                 System.exit(1);
             }
-            finally
-            {
+            finally {
                 // Close local socket and output file
                 tftpClient.close();
-                try
-                {
+                try {
                     if (output != null) {
                         output.close();
                     }
                     closed = true;
                 }
-                catch (IOException e)
-                {
+                catch (IOException e) {
                     closed = false;
                     System.err.println("Error: error closing file.");
                     System.err.println(e.getMessage());
@@ -161,17 +145,14 @@ public class TFTPClientTest {
                 System.exit(1);
             }
 
-        } else {
-            // We're sending a file
+        } else {//send a file
             FileInputStream input = null;
 
             // Try to open local file for reading
-            try
-            {
+            try {
                 input = new FileInputStream(localFilename);
             }
-            catch (IOException e)
-            {
+            catch (IOException e) {
                 tftpClient.close();
                 System.err.println("Error: could not open local file for reading.");
                 System.err.println(e.getMessage());
@@ -179,36 +160,30 @@ public class TFTPClientTest {
             }
 
             // Try to send local file via TFTP
-            try
-            {
+            try {
                 tftpClient.sendFile(remoteFilename, mode, input, InetAddress.getByName(hostname));
             }
-            catch (UnknownHostException e)
-            {
+            catch (UnknownHostException e) {
                 System.err.println("Error: could not resolve hostname.");
                 System.err.println(e.getMessage());
                 System.exit(1);
             }
-            catch (IOException e)
-            {
+            catch (IOException e) {
                 System.err.println(
                         "Error: I/O exception occurred while sending file.");
                 System.err.println(e.getMessage());
                 System.exit(1);
             }
-            finally
-            {
+            finally {
                 // Close local socket and input file
                 tftpClient.close();
-                try
-                {
+                try {
                     if (input != null) {
                         input.close();
                     }
                     closed = true;
                 }
-                catch (IOException e)
-                {
+                catch (IOException e) {
                     closed = false;
                     System.err.println("Error: error closing file.");
                     System.err.println(e.getMessage());
--- src/com/bgoverseas/TFTPServerTest.java
+++ src/com/bgoverseas/TFTPServerTest.java
@@ -7,26 +7,30 @@ import java.io.InputStreamReader;
 
 /**
  * Created by binwang on 17/08/15.
+ * TFTP server used for testing
  */
 public class TFTPServerTest {
+    /**
+     * entry of server test
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception
     {
         if (args.length != 1)
         {
-        System.out
-        .println("You must provide 1 argument - the base path for the server to serve from.");
-        System.exit(1);
+            System.out
+            .println("One parameter is required which is the directory for the test server");
+            System.exit(1);
         }
 
-        TFTPServer ts = new TFTPServer(new File(args[0]), new File(args[0]), TFTPServer.ServerMode.R);
+        TFTPServer ts = new TFTPServer(new File(args[0]), new File(args[0]), TFTPServer.ServerMode.W);
         ts.setSocketTimeout(2000);
 
-
-        System.out.println("TFTP Server running.  Press enter to stop.");
+        System.out.println("TFTP Server running. Press enter to stop.");
         new InputStreamReader(System.in).read();
-
         ts.shutdown();
-        System.out.println("Server shut down.");
+        System.out.println("Server shutting down.");
         System.exit(0);
     }
 }
